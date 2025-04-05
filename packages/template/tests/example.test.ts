f849ff98e4dfdce9b8db1363662847228ff8053c
Mavricus <amamakc61@gmail.com>
2025-04-05 19:43:13 +0200

    feat(scripts): add package creation script with template improvements
    - Add create-package script
    - Update template package `README.md`

--- packages/template/tests/example.test.ts
+++ packages/template/tests/example.test.ts
@@ -1,5 +1,7 @@
-describe('Example', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+import { sum } from '../src/index.js';
+
+describe('sum', () => {
+  it('should return the sum of two numbers', () => {
+    expect(sum(1, 2)).toBe(3);
   });
 });
